9be51abae7f9ab346d60a4e5c57143e894e8ca72
UrbanCoffee <rudy.m.7854@gmail.com>
2021-05-21 00:12:47 -0700

    Changes to Proto/Main

--- proto/main.cpp
+++ proto/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
-#include <sstream>
+//#include <sstream>
 #include <vector>
 #include <iomanip>
 
@@ -10,9 +10,11 @@ struct obj{
     std::vector<float> data;
 };
 
-int evaluation(obj*) {return ( rand() % 1000 ) + 1;}
+double evaluation(std::vector<obj>*) {return ( ( rand() % 1000 ) + 1) / 10.0;}
 std::vector<obj>* dataPreprocessing(std::fstream&, int); 
 std::vector<int8_t> feature_search(std::vector<obj>*, int, int8_t); // {0,1,0,1,0,1,0} -> features {1, 3, 5} ; {0,0,1,1,0,0,1} -> features {2,3,6}
+
+std::string printFeatures(std::vector<int8_t>&);
 
 int8_t getFeatureAlg(); 
 int getFeatureSetLength();
@@ -20,15 +22,13 @@ int getFeatureSetLength();
 int main() {
     srand(time(NULL));
     std::string file_name;
-    std::cout << "Welcome to Urban's Feature Selection Algorithm\n\n";
+    std::cout << "Welcome to Rudy and Sana's Feature Selection Algorithm\n\n";
     int featureSetLength = getFeatureSetLength(); std::cout << std::endl;
     std::cout << "Type in the name of the file to test: ";
     std::cin >> file_name; std::cout << std::endl;
     
-    std::fstream dataFile (file_name.c_str(), std::fstream::in);
-    if( dataFile.is_open() ){
-        std::cout << ">>Opened " << file_name << std::endl;
-        
+    std::fstream dataFile;//std::fstream dataFile (file_name.c_str(), std::fstream::in);
+    if( true /* dataFile.is_open() */ ){
         std::vector<obj>* dataSet = dataPreprocessing(dataFile, featureSetLength);
         int8_t choice_1 = getFeatureAlg();
 
@@ -39,7 +39,7 @@ int main() {
         dataFile.close();
     }
     else{
-        std::cout << ">>Cannot find \"" << file_name << "\" in current directory.\n";
+        std::cout << ">>Cannot find or open \"" << file_name << "\" in current directory.\n";
     }
 
     return 0;
@@ -53,7 +53,8 @@ std::vector<int8_t> feature_search(std::vector<obj>* dataSet, int fSetLength, in
     if(choice == 0 || choice == 1){ // we can combine the algorithms for Foward Select and Backward Elim. using <choice> to flip the required variables
         fSet = std::vector<int8_t> (fSetLength+1, choice);
 
-        maxAccuracy = evaluation(nullptr) / 10.0; // maxAccuracy = evaluation(dataSet);
+        maxAccuracy = evaluation(dataSet);
+
         if(choice == 0) {
             std::cout << "Using no features and \"random\" evaluation, accuracy is " << std::setprecision(3) << maxAccuracy << "%\n\n";
         }
@@ -71,13 +72,9 @@ std::vector<int8_t> feature_search(std::vector<obj>* dataSet, int fSetLength, in
                 if(fSet[i] == choice){
                     fSet[i] = (choice+1)%2; // if 1 -> flip to 0 ; if 0 -> flip to 1
 
-                    currAccuracy = evaluation(nullptr) / 10.0; // currAccuracy = evaluation(dataSet);
+                    currAccuracy = evaluation(dataSet);
 
-                    std::cout << "Using feature(s) {";
-                    for(unsigned int j = 1; j < fSet.size(); ++j){
-                        if(fSet[j] == 1){ std::cout << j << ',';}
-                    }
-                    std::cout << "\b} accuracy is " << std::setprecision(3) << currAccuracy << "%\n";
+                    std::cout << "Using feature(s) {" << printFeatures(fSet) << "} accuracy is " << std::setprecision(3) << currAccuracy << "%\n";
 
                     if(maxAccuracy < currAccuracy){
                         maxIndex = i;
@@ -88,12 +85,7 @@ std::vector<int8_t> feature_search(std::vector<obj>* dataSet, int fSetLength, in
             }
             if(maxIndex != 0){
                 fSet[maxIndex] = (choice+1)%2;
-
-                std::cout << "\nFeature set {";
-                for(unsigned int j = 1; j < fSet.size(); ++j){
-                    if(fSet[j] == 1){ std::cout << j << ',';}
-                }
-                std::cout << "\b} was best, accuracy is " << std::setprecision(3) << maxAccuracy << "%\n\n";
+                std::cout << "\nFeature set {" << printFeatures(fSet) << "} was best, accuracy is " << std::setprecision(3) << maxAccuracy << "%\n\n";
             }
             else{
                 std::cout << "\n(Warning, Accuracy has decreased!)\n";
@@ -105,11 +97,7 @@ std::vector<int8_t> feature_search(std::vector<obj>* dataSet, int fSetLength, in
     }
 
     if(fSet.size() > 1){
-        std::cout << "Finished search!! The best feature subset is {"; 
-        for(unsigned int j = 1; j < fSet.size(); ++j){
-            if(fSet[j] == 1){ std::cout << j << ',';}
-        }
-        std::cout << "\b} which has an accuracy of " << std::setprecision(3) << maxAccuracy << "%\n";
+        std::cout << "Finished search!! The best feature subset is {" << printFeatures(fSet) << "} which has an accuracy of " << std::setprecision(3) << maxAccuracy << "%\n";
     } else {
         std::cout << "Finished search!! The best feature subset has no features which has an accuracy of " << std::setprecision(3) << maxAccuracy << "%\n";
     }
@@ -139,8 +127,20 @@ std::vector<obj>* dataPreprocessing(std::fstream& fs, int fSetLength) {
      / --> normalization <-- /
 
     return dataSet; */
-    std::cout << ">>Entered DataProc.\n";
     return nullptr;
+}
+
+std::string printFeatures(std::vector<int8_t>& fSet){
+    std::string str_fList;
+    for(unsigned int i = 1; i < fSet.size(); ++i){
+        if(fSet[i] == 1){ str_fList += std::to_string(i) + ',';}
+    }
+
+    if(str_fList.size() != 0){
+        str_fList.pop_back();
+    }
+
+    return str_fList;
 }
 
 int8_t getFeatureAlg(){
@@ -150,11 +150,11 @@ int8_t getFeatureAlg(){
     int8_t choice = 0;
     std::cout << "Type the number of the algorithm you want to run:\n"
               << "    [1] Forward Selection\n"
-              << "    [2] Backward Elimination\n"
-              << "    [3] Urban's Homegrown Algorithm\n";
+              << "    [2] Backward Elimination\n";
+             // << "    [3] Urban's Homegrown Algorithm\n";
     while(input == ""){
         std::cout << ">>Input your choice: "; std::getline(std::cin, input);
-        if(input == "1" || input == "2" || input == "3") {choice = input.at(0) - 49;}
+        if(input == "1" || input == "2" /*|| input == "3"*/) {choice = input.at(0) - 49;}
         else{
             std::cout << ">>SELECT PROPER INPUT\n";
             input = "";
